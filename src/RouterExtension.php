09cd6e342c3ec5b5c2ecf826359cef4183b15497
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2026-03-24 13:12:51 +0100

    Updated extensions for fhooe/router 3.0.0

--- src/RouterExtension.php
+++ src/RouterExtension.php
@@ -9,14 +9,13 @@ use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 /**
- * Twig extension that allows to use the fhooe/router functions urlFor() and getBasePath() within a Twig template.
+ * Twig extension that exposes fhooe/router's urlFor() and the read-only basePath in templates as url_for and get_base_path.
  * @package Fhooe\Twig
  * @author Wolfgang Hochleitner <wolfgang.hochleitner@fh-hagenberg.at>
  * @since 1.0.0
  */
 final class RouterExtension extends AbstractExtension
 {
-
     /**
      * Creates a new Router extension with the fhooe/router object as parameter.
      * @param Router $router The fhooe/router object.
@@ -24,14 +23,14 @@ final class RouterExtension extends AbstractExtension
     public function __construct(private readonly Router $router) {}
 
     /**
-     * Provides the router methods urlFor() and getBasePath() as Twig functions url_for() and get_base_path().
+     * Provides url_for(pattern) and get_base_path() for Twig templates (router uses public readonly basePath).
      * @return TwigFunction[] The array with Twig functions.
      */
     public function getFunctions(): array
     {
         return [
-            new TwigFunction("url_for", [$this->router, "urlFor"]),
-            new TwigFunction("get_base_path", [$this->router, "getBasePath"]),
+            new TwigFunction("url_for", $this->router->urlFor(...)),
+            new TwigFunction("get_base_path", fn(): string => $this->router->basePath),
         ];
     }
 }
